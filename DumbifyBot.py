307e8390a6f3a9ff56ad442938e2201dd55059d8
Pj <47722275+Pj6595@users.noreply.github.com>
2021-07-13 12:39:18 +0200

    Update DumbifyBot.py

--- DumbifyBot.py
+++ DumbifyBot.py
@@ -69,8 +69,7 @@ def main():
     # dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path='1704982427:AAFL55HPUrj65dEG3mxraxbsNsqCGLINGp8')
-    updater.bot.setWebhook('https://desolate-citadel-02620.herokuapp.com/' + '1704982427:AAFL55HPUrj65dEG3mxraxbsNsqCGLINGp8')
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
